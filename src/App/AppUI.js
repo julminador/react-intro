8ff9019bb0526a782903bb625f27531c42224c1b
julminador <juliansosa021@gmail.com>
2023-05-02 19:09:10 -0500

    Integración de funcionalidad CreateTodo

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -6,6 +6,7 @@ import { TodoList } from "../TodoList";
 import { TodoItem } from "../TodoItem";
 import { CreateTodoButton } from "../CreateTodoButton/";
 import { Modal } from "../Modal/";
+import { TodoForm } from "../TodoForm/";
 
 function AppUI() {
   const {
@@ -41,7 +42,7 @@ function AppUI() {
 
       {openModal && (
       <Modal>
-        <p>Hola</p>
+        <TodoForm />
       </Modal>
       )}
       <CreateTodoButton
